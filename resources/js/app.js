a257c4734fbd39a5930b5ecb01dc3fffca0e553b
Wolfgang Meier <wolfgangmm@gmail.com>
2014-07-15 14:27:12 +0200

    Hide table of contents and move it in from the left if user wants to see it.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,9 @@
 * put application-specific JavaScript code
 */
 'use strict';
+
+$(document).ready(function() {
+    $("#toc-toggle").click(function(ev) {
+        $(".sidebar-offcanvas").parent().toggleClass("active");
+    });
+});
